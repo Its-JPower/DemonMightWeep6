ceba5b1fd629cc95861837015c53796476006044
SHADOW-BORN <rivmor65@kingshigh.school.nz>
2026-05-01 12:19:51 +1200

    Donte Animations imported

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -14,7 +14,8 @@ const DASH_SPEED := 15.0
 @onready var _camera_pivot_yaw : Node3D = %CameraPivotYaw
 @onready var _camera_pivot_pitch: Node3D = %CameraPivotPitch
 @onready var state_machine: StateMachine = $StateMachine
-@onready var player_model: Node3D = $Mesh
+@onready var player_model: Node3D = $Donte_With_Animations
+@onready var anim_player = $Donte_With_Animations/AnimationPlayer
 
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.0025
 @export var tilt_limit = deg_to_rad(75)
@@ -28,6 +29,17 @@ var rotation_mode := RotationMode.MOVEMENT
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	state_machine.init(self)
+	_fix_ual1_tracks()
+
+func _fix_ual1_tracks() -> void:
+	for lib_name in anim_player.get_animation_library_list():
+		var lib = anim_player.get_animation_library(lib_name)
+		for anim_name in lib.get_animation_list():
+			var anim = lib.get_animation(anim_name)
+			for i in anim.get_track_count():
+				var path = str(anim.track_get_path(i))
+				if path.begins_with("Armature/"):
+					anim.track_set_path(i, path.replace("Armature/", ""))
 
 func _input(event: InputEvent) -> void:
 	state_machine.handle_input(event)
@@ -49,20 +61,17 @@ func _physics_process(delta: float) -> void:
 	match rotation_mode:
 		RotationMode.MOVEMENT: rotate_model_toward_movement(delta)
 		RotationMode.CAMERA:   rotate_model_toward_camera(delta)
-		RotationMode.LOCKED:   pass  # state handles it or holds last rotation
+		RotationMode.LOCKED:   pass
 	print(rotation_mode)
 
 func get_movement_input() -> Vector3:
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var forward = -_camera_pivot_yaw.global_transform.basis.z
 	var right = _camera_pivot_yaw.global_transform.basis.x
-
 	forward.y = 0
 	forward = forward.normalized()
 	right.y = 0
 	right = right.normalized()
-
 	return (forward * -input_dir.y + right * input_dir.x).normalized()
 
 func apply_gravity(delta: float) -> void:
@@ -74,19 +83,19 @@ func apply_movement(delta: float) -> void:
 	var target_speed = RUN_SPEED if is_sprinting else WALK_SPEED
 
 	if direction.length() > 0.1:
-		# Accelerate toward target velocity
 		var target_velocity = direction * target_speed
 		velocity.x = move_toward(velocity.x, target_velocity.x, ACCELERATION * delta)
 		velocity.z = move_toward(velocity.z, target_velocity.z, ACCELERATION * delta)
+		if is_sprinting:
+			anim_player.play("Stash 2/Sprint")
+		else:
+			anim_player.play("Stash 2/Walk")
 	else:
-		# Decelerate to a stop
 		velocity.x = move_toward(velocity.x, 0.0, DECELERATION * delta)
 		velocity.z = move_toward(velocity.z, 0.0, DECELERATION * delta)
 
 func rotate_model_toward_movement(delta: float) -> void:
 	var direction = get_movement_input()
-	
-	# Use input direction if available, otherwise fall back to current velocity direction
 	var rotate_toward: Vector3
 	if direction.length() > 0.1:
 		rotate_toward = direction
@@ -95,8 +104,7 @@ func rotate_model_toward_movement(delta: float) -> void:
 		if horizontal_velocity.length() > 0.1:
 			rotate_toward = horizontal_velocity.normalized()
 		else:
-			return  # Fully stopped, no rotation needed
-	
+			return
 	var target_basis = Basis.looking_at(rotate_toward, Vector3.UP)
 	player_model.global_basis = player_model.global_basis.slerp(target_basis, ROTATION_SPEED * delta)
 
@@ -109,6 +117,6 @@ func rotate_model_toward_camera(delta: float) -> void:
 	var forward = -_camera_pivot_yaw.global_transform.basis.z
 	forward.y = 0
 	if forward.length() < 0.01:
-		return  # guard against zero vector crash
+		return
 	var target_basis = Basis.looking_at(forward, Vector3.UP)
 	player_model.global_basis = player_model.global_basis.slerp(target_basis, ROTATION_SPEED * delta)
